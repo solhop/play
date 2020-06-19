b63e65126d39453e01996d6c6089b9efc8329044
Prateek Kumar <prateek@prateekkumar.in>
2020-06-19 23:19:59 +0530

    Add rsat as dependency
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,70 @@ pub fn greet() {
     utils::set_panic_hook();
     log_str("Hello, play!");
 }
+
+#[wasm_bindgen]
+pub fn sat_solver(input: String) -> String {
+    // Called to log the panic error messages to console.err
+    utils::set_panic_hook();
+
+    let mut reader = std::io::BufReader::new(input.as_bytes());
+    let parsed = rsat::parser::parse_dimacs_from_buf_reader(&mut reader);
+    let (n_vars, clauses) = if let rsat::parser::Dimacs::Cnf { n_vars, clauses } = parsed {
+        (n_vars, clauses)
+    } else {
+        panic!("Incorrect input format");
+    };
+
+    use rsat::cdcl::*;
+    use rsat::Var;
+
+    let options = SolverOptions::default();
+    let mut solver = Solver::new(options);
+
+    let vars: Vec<Var> = (0..n_vars).map(|_| solver.new_var()).collect();
+
+    for clause in clauses {
+        let lits = clause
+            .into_iter()
+            .map(|l| {
+                let var = vars[(l.abs() - 1) as usize];
+                if l < 0 {
+                    var.neg_lit()
+                } else {
+                    var.pos_lit()
+                }
+            })
+            .collect();
+        solver.add_clause(lits);
+    }
+
+    let solution = solver.solve(vec![]);
+
+    use rsat::Solution::*;
+    match solution {
+        Unsat => "s UNSATISFIABLE\n".to_owned(),
+        Unknown => "s UNKNOWN\n".to_owned(),
+        Best(solution) => {
+            let mut output = String::new();
+            output += "s UNKNOWN\n";
+            let solution = solution.iter().map(|&x| if x { 1 } else { -1 });
+            output += "v ";
+            for (i, v) in solution.enumerate() {
+                output += &format!("{} ", v * ((i + 1) as i32));
+            }
+            output += "0\n";
+            output
+        }
+        Sat(solution) => {
+            let mut output = String::new();
+            output += "s SATISFIABLE\n";
+            output += "v ";
+            let solution = solution.iter().map(|&x| if x { 1 } else { -1 });
+            for (i, v) in solution.enumerate() {
+                output += &format!("{} ", v * ((i + 1) as i32));
+            }
+            output += "0\n";
+            output
+        }
+    }
+}
--- www/index.js
+++ www/index.js
@@ -1,3 +1,6 @@
 import * as wasm from "play";
 
 wasm.greet();
+
+let input = "p cnf 2 3\n1 0\n-1 2 0\n2 0\n";
+console.log(wasm.sat_solver(input));
